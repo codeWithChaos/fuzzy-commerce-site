a70630aebfb2547e19ada3891e36d77badd2fc0f
codewithchaos <codewithchaos1809@gmail.com>
2024-11-26 13:50:12 +0000

    Position the cards in the template to properly render it's contents.

--- store/models.py
+++ store/models.py
@@ -27,6 +27,8 @@ class Product(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
     description = models.TextField()
     image = models.ImageField(upload_to='uploads/products/')
+    is_sale = models.BooleanField(default=False)
+    sale_price = models.DecimalField(default=0, max_digits=6, decimal_places=2)
 
     def __str__(self):
         return self.name
@@ -35,7 +37,7 @@ class Order(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
     quantity = models.IntegerField(default=1)
-    price = models.DecimalField(max_digits=6, decimal_places=2)
+    # price = models.DecimalField(max_digits=6, decimal_places=2)
     address = models.CharField(max_length=100, default='', blank=True)
     phone = models.CharField(max_length=15, default='', blank=True)
     date = models.DateField(default=datetime.datetime.today)
