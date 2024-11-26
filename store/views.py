bd70dba3dbf933bbe7e7c948072e31695c77f459
codewithchaos <codewithchaos1809@gmail.com>
2024-11-26 15:34:35 +0000

    Implement the login & logout functionality.

--- store/views.py
+++ store/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Product
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 
+@login_required(login_url='/login/')
 def home(request):
     products = Product.objects.all()
     context = {
@@ -11,3 +15,23 @@ def home(request):
 
 def about(request):
     return render(request, 'store/about.html')
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, 'You have been logged in.')
+            return redirect('home')
+        else:
+            messages.error(request, 'Invalid username or password.')
+            return redirect('login')
+    else:
+        return render(request, 'store/login.html')
+
+def logout_view(request):
+    logout(request)
+    messages.success(request, 'You have been logged out. Thanks for stopping by! :D')
+    return redirect('login')
