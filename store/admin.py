87922f83e8fe28bf4205941d67c9da0be40266fd
codewithchaos <codewithchaos1809@gmail.com>
2024-12-05 14:47:30 +0000

    Add user profile extension.

--- store/admin.py
+++ store/admin.py
@@ -1,8 +1,27 @@
 from django.contrib import admin
-from .models import Category, Product, Customer, Order
+from .models import Category, Product, Customer, Order, Profile
+from django.contrib.auth.models import User
 
 
 admin.site.register(Category)
 admin.site.register(Product)
 admin.site.register(Customer)
 admin.site.register(Order)
+admin.site.register(Profile)
+
+
+# Mix profile info and user info
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    
+# Extend the User model
+class UserAdmin(admin.ModelAdmin):
+    model = User
+    fields = ['username', 'first_name', 'last_name', 'email']
+    inlines = [ProfileInline]
+    
+# Unregister the old way
+admin.site.unregister(User)
+
+# Re-Register the new way
+admin.site.register(User, UserAdmin)
