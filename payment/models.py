5275b0edcf02521e181a184a00ed17dce32fb2fa
codewithchaos <codewithchaos1809@gmail.com>
2024-12-12 18:14:18 +0000

    Alter the shipping Address models to use string representation and also add verbose name plural to make the name sensible🤣🤣. Also make sure Shipping Adress model is registered in the admin backend stuff🙄.

--- payment/models.py
+++ payment/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class ShippingAddress(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    full_name = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    address1 = models.CharField(max_length=255)
+    address2 = models.CharField(max_length=255)
+    city = models.CharField(max_length=200, blank=True, null=True)
+    zipcode = models.CharField(max_length=200, blank=True, null=True)
+    country = models.CharField(max_length=200)
+
+    class Meta:
+        verbose_name_plural = 'Shipping Address'
+
+    def __str__(self):
+        return f"Shipping Address - {str(self.id)}"
+
